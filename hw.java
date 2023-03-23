e02b2da6226c88c6b97da148d4141c38e848ab12
yurik1m <yuri.k.tech@gmail.com>
2023-03-23 13:52:54 +0900

    2023-03-23 실습과제#4 3번

---
+++ hw.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        System.out.print("이름을 입력하세요: ");
+        String name = sc.next();
+        System.out.print("학번을 입력하세요: ");
+        int id = sc.nextInt();
+		
+        System.out.println("<출력>");
+        System.out.println("이름 : " + name);
+        System.out.println("학번 : " + id);
+	}
+
+}
